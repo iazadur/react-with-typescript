f06b0c79955fe019de325b7b68494448016d979f
iazadur <iamazadur@gmail.com>
2022-01-12 16:48:05 +0600

    create a list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import AddToList from './components/AddToList';
+import List from './components/List';
+
+interface IState {
+  people: {
+    name: string
+    age: number
+    url: string
+    note?: string
+  }[]
+}
+
+// const user = [
+//   {
+//     name:"md azadur",
+//     age:90,
+//     url:"",
+//     note:"hello this is note for yours"
+//   }
+// ]
 
 function App() {
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "md azadur",
+      age: 90,
+      url: "",
+      note: "hello this is note for yours"
+    }
+  ])
+  // setPeople(user)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* {
+        people.map((u, id) => <List people={u} key={id}/>)
+      } */}
+      <h1>invite you for the page</h1>
+      <List people={people} />
+      <AddToList />
     </div>
   );
 }
---
+++ src/components/List.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+interface IProps {
+    people: {
+        name: string
+        age: number
+        url: string
+        note?: string
+    }[]
+}
+
+const List: React.FC<IProps> = ({ people }) => {
+
+    const renderList = (): JSX.Element[] => {
+        return people.map(person => {
+            return (
+                <li style={{ display: "flex", justifyContent: "space-between" }}>
+                    <div>{person.name}</div>
+                    <div>{person.age} YEARS OLD</div>
+                    <div>{person.note}</div>
+                </li>
+            )
+        })
+    }
+    return (
+        <div className='wi'>
+            {renderList()}
+        </div>
+    )
+}
+export default List
